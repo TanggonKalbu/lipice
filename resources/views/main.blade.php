3b1d22cce9f06eb95be114bd40ade2786109b439
bayuharisaputro <bayuharisaputro@gmail.com>
2018-08-20 10:50:03 +0700

    lipice

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,278 +1,42 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Lipice</title>
-
-
-      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-    <style>
-        img {
-    width: 100%;
-    height: auto;
-}
-        *{
-            padding : 0px;
-            margin : 0px;
-            width: 100%;
-        }
-        #Table_01 {
-            position:absolute;
-            left:0px;
-            top:0px;
-            align : center;
-            width:1366px;
-            height:3100px;
-        }
-
-        #summercamp--2--01 {
-            position:absolute;
-            left:0px;
-            top:0px;
-            width:631px;
-            height:84px;
-        }
-
-        #logo-girl {
-            position:absolute;
-            left:631px;
-            top:0px;
-            width:735px;
-            height:820px;
-        }
-
-        #summercamp--2--03 {
-            position:absolute;
-            left:0px;
-            top:84px;
-            width:86px;
-            height:1383px;
-        }
-
-        #icon {
-            position:absolute;
-            left:86px;
-            top:84px;
-            width:545px;
-            height:454px;
-        }
-
-        #summercamp--2--05 {
-            position:absolute;
-            left:86px;
-            top:538px;
-            width:545px;
-            height:282px;
-        }
-
-        #card {
-            position:absolute;
-            left:86px;
-            top:820px;
-            width:1182px;
-            height:647px;
-        }
-
-        #summercamp--2--07 {
-            position:absolute;
-            left:1268px;
-            top:820px;
-            width:98px;
-            height:647px;
-        }
-
-        #girl-kiri {
-            position:absolute;
-            left:0px;
-            top:1467px;
-            width:245px;
-            height:784px;
-        }
-
-        #summercamp--2--09 {
-            position:absolute;
-            left:245px;
-            top:1467px;
-            width:935px;
-            height:141px;
-        }
-
-        #girl-kanan {
-            position:absolute;
-            left:1180px;
-            top:1467px;
-            width:186px;
-            height:99px;
-        }
-
-        #girl-kanan-1 {
-            position:absolute;
-            left:1180px;
-            top:1566px;
-            width:186px;
-            height:801px;
-        }
-
-        #timeline {
-            position:absolute;
-            left:245px;
-            top:1608px;
-            width:935px;
-            height:1299px;
-        }
-
-        #girl-kiri-1 {
-            position:absolute;
-            left:0px;
-            top:2251px;
-            width:245px;
-            height:540px;
-        }
-
-        #girl-kanan014 {
-            position:absolute;
-            left:1180px;
-            top:2367px;
-            width:186px;
-            height:540px;
-        }
-
-        #girl-kiri015 {
-            position:absolute;
-            left:0px;
-            top:2791px;
-            width:245px;
-            height:116px;
-        }
-
-        #footer {
-            position:absolute;
-            left:0px;
-            top:2907px;
-            width:1366px;
-            height:193px;
-        }
-        #buah{
-            float:right;
-        }
-
-.column {
-    float: left;
-    width: 33.33%;
-    padding: 5px;
-}
-
-/* Clearfix (clear floats) */
-.row::after {
-    content: "";
-    clear: both;
-    display: table;
-}
-.kiri-logo{
-    width:545px; 
-    height:454px; 
-    float:left;
-    margin-top:-140px;
-    padding-left:150px;
-}
-.kanan-ballon{
-    margin-top:-190px;
-    width:735px; 
-    height:820px; 
-    float:right;
-}
-.card {
-    box-shadow: 0px 4px 8px 0px rgba(0,0,0,0.2);
-    transition: 0.3s;
-    width: 60%;
-    background-color:white;
-    align:center;
-    margin-left:60px;   
-}
-
-.card:hover {
-    box-shadow: 0px 4px 8px 0px rgba(0,0,0,0.2);
-}
-
-.container {
-    padding: 140px 85px;
-}
-
-.textatas{
-    color: red;
-    font-size: 20pt;
-    font-family: arial;
-    margin-left: 150px;
-
-}
-    </style>
+  <title>Firebase Phone Number Auth</title>
 </head>
-<body style="background-color:#fbdee8; margin-top: 0px; margin-bottom: 0px; margin-left: 0px; margin-right: 0px;">
-<!-- Save for Web Slices (summercamp (2).psd) -->
-<div class="container-fluid">
-
-
-	<!-- buah atas -->
-    <div class="row">
-        <div class="column">
-            <img src="images/buah_kiri_atas.png" style="width:121px; height:221px; float:left;" alt="">
-        </div>
-        <div class="column">
-            <img src="images/buah_kanan_atas.png" style="width:109px; height:128px; float:right;" alt="">
-        </div>
-    </div>
-
-    <!-- header -->
-     <div class="row">
-        <div class="column">
-            <img src="images/Untitled-6.png" alt="" class="kiri-logo">
-        </div>
-        <div class="column">
-            <img src="images/Untitled-7.png" alt="" class="kanan-ballon">
-        </div>
-    </div>
-
-    <div class="row">
-        <h2 class="textatas"><b>LOREM IPSUM DOLOR SIT <br> 
-        AMET, CONSECTETUR <br> 
-        ADIPISINING ELIT. ED O <br> 
-        EIUSMOD TEMPOR INCINDINTU</b></h2>
-    </div>
-
-    <!-- form -->
-    <!-- <div class="card">
-        <div class="container">
-            <h4><b>Calling all beauty enthusiast!</b></h4>
-            <h4><b>Submission are available to anyone  and everyone with a passion</b></h4>
-            <h4><b>for make-up artistry.</b></h4> 
-            <p>REGISTRATION FIELD</p> 
-            <br>
-        </div>
-    </div> -->
-
-
-    <!-- Card -->
-    <div class="card" style="">
-    <div class="card-body">
-        <h5 class="card-title">Calling all beauty enthusiast!</h5>
-        <h5 class="card-title">Submission are available to anyone  and everyone with a passion</h5>
-        <h5 class="card-title">for make-up artistry.</h5>
-        <br>
-        <h6 class="card-subtitle mb-2 text-muted">REGISTRATION FIELDS</h6>
-        <br>
-        <form action="">
-
-        </form>
-        <p class="card-text">Quick sample text to create the card title and make up the body of the card's content.</p>
+<body>
+  <form>
+    <input type="text" id="verificationcode" value="enter verification">
+    <input type="button" value="Submit" onclick="myFunction()">
+  </form>
+  <div id="recaptcha-container"></div>
+  <script src="https://www.gstatic.com/firebasejs/4.8.1/firebase.js"></script>
+  <script type="text/javascript">
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyA9q1pskVgdzJbZ3Qki_0UuYM9L5bkQF7w",
+    authDomain: "lipice-8a856.firebaseapp.com",
+    databaseURL: "https://lipice-8a856.firebaseio.com",
+    projectId: "lipice-8a856",
+    storageBucket: "lipice-8a856.appspot.com",
+    messagingSenderId: "894497846646"
+  };
+  firebase.initializeApp(config);
+</script>
+<script type="text/javascript">
+  window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('recaptcha-container');
+  firebase.auth().signInWithPhoneNumber("+62081945314191", window.recaptchaVerifier) 
+  .then(function(confirmationResult) {
+    window.confirmationResult = confirmationResult;
+    console.log(confirmationResult);
+  });
+  var myFunction = function() {
+    window.confirmationResult.confirm(document.getElementById("verificationcode").value)
+    .then(function(result) {
       
-    </div>
-    </div>
-
-<!-- End Save for Web Slices -->
+    }, function(error) {
+      console.log(error);
+    });
+  };
+  </script>
 </body>
 </html>
