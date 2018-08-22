112f8b7908aa3070c630290e99f11da5defb8130
aurakanzaa <aurakanzaa@yahoo.co.id>
2018-08-22 19:16:18 +0700

    belum kelar

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -276,6 +276,36 @@ body{
 <!-- Save for Web Slices (summercamp (2).psd) -->
 <div>
 
+<br>
+  <!-- Button to Open the Modal -->
+  <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
+    Open modal
+  </button>
+
+  <!-- The Modal -->
+  <div class="modal fade" id="myModal">
+    <div class="modal-dialog modal-lg">
+      <div class="modal-content">
+      
+        <!-- Modal Header -->
+        <div class="modal-header">
+          <h4 class="modal-title">Modal Heading</h4>
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+        </div>
+        
+        <!-- Modal body -->
+        <div class="modal-body">
+          Modal body..
+        </div>
+        
+        <!-- Modal footer -->
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        </div>
+        
+      </div>
+    </div>
+  </div>
 
 	<!-- buah atas -->
             <img src="images/buah_kiri_atas.png" style="width:121px; height:221px; float:left;" alt="">
